2e1a678a80ca8d9e6601bb2fe76e855844cbbaf1
Максим Киржаев <kirzhaefff@yandex.ru>
2024-06-12 22:25:48 +0300

    added 5th step (+logic and brain_even(main))

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,24 +1,9 @@
-import random
-import prompt
+from brain_games.logic import brain_even
 
 
-def brain_even():
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    index = 0
-    while index < 3:
-        number = random.randint(1, 100)
-        print(f'Question: {number}')
-        answer = prompt.string('Your answer: ')
-        if (answer == 'yes' and number % 2 == 0) and (answer == 'no' and number % 2 == 1):
-            print('Correct!')
-        elif (answer != 'yes' and number % 2 == 0):
-            print(f"'{answer}' is wrong answer ;(. Correct answer was 'yes'.\nLet`s try again,{name}!")
-        elif (answer != 'no' and number % 2 == 1):
-            print(f"'{answer}' is wrong answer ;(. Correct answer was 'no'.\nLet`s try again,{name}")
-    print(f'Congratulations, {name}!')
+def main():
+    brain_even()
+
 
 if __name__ == '__main__':
-    brain_even()
+    main()
