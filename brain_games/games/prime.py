90008f9d48ab3c78f2e195fcb1daa4dc99383158
Максим Киржаев <kirzhaefff@yandex.ru>
2024-06-16 16:30:59 +0300

    added 9th step

---
+++ brain_games/games/prime.py
@@ -0,0 +1,22 @@
+from random import randint
+
+
+EXPLAIN = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+def is_prime(a):
+    if a < 2:
+        return False
+    for i in range(2, a // 2 + 1):
+        if a % i == 0:
+            return False
+    return True
+
+
+def brain():
+    number = randint(1, 100)
+    question = f'Question: {number}'
+    if is_prime(number):
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
+    return question, correct_answer
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,9 @@
+from brain_games.logic import logic
+from brain_games.games import prime
+
+def main():
+    logic(prime)
+
+
+if __name__ == '__main__':
+    main()
