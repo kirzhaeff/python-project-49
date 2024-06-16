d1e917bae1c106289bead16785a16014573be78a
Максим Киржаев <kirzhaefff@yandex.ru>
2024-06-16 16:50:53 +0300

    fix

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from brain_games.logic import logic
 from brain_games.games import even
 
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python3
 from brain_games.logic import logic
 from brain_games.games import prime
+
 
 def main():
     logic(prime)
