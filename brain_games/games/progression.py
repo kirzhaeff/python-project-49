e1e2d3662452f26d8eab389c1b5e0d7b3953a6ad
Максим Киржаев <kirzhaefff@yandex.ru>
2024-07-02 23:42:26 +0300

    added get_progression (logic) #2

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -10,12 +10,19 @@ STEP_MIN = 3
 STEP_MAX = 5
 
 
+def get_progression(start_progression, step, length_progression):
+    end_progression = start_progression + (step * length_progression)
+    numbers = []
+    for i in range(start_progression, end_progression, step):
+        numbers.append(i)
+    return numbers
+
+
 def generate_round():
     start_progression = randint(START_MIN, START_MAX)
     length_progression = randint(LENGTH_MIN, LENGTH_MAX)
     step = randint(STEP_MIN, STEP_MAX)
-    end_progression = start_progression + (step * length_progression)
-    progression = list(range(start_progression, end_progression, step))
+    progression = get_progression(start_progression, step, length_progression)
     random_number = randint(0, length_progression - 1)
     correct_answer = str(progression[random_number])
     progression[random_number] = '..'
