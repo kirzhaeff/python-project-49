c387c98637d01ca563fc6d6ef485837bb3bfaeee
Максим Киржаев <kirzhaefff@yandex.ru>
2024-07-03 00:02:12 +0300

    added calculate (func) #2

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -6,15 +6,21 @@ START_NUM = 1
 END_NUM = 30
 
 
+def calculate(num_1, num_2, operator):
+
+    if operator == '+':
+        result = num_1 + num_2
+    elif operator == '-':
+        result = num_1 - num_2
+    elif operator == '*':
+        result = num_1 * num_2
+    return result
+
+
 def generate_round():
     operator = random.choice(['+', '-', '*'])
     num_1 = random.randint(START_NUM, END_NUM)
     num_2 = random.randint(START_NUM, END_NUM)
     question = f'Question: {num_1} {operator} {num_2}'
-    if operator == '+':
-        correct_answer = num_1 + num_2
-    elif operator == '-':
-        correct_answer = num_1 - num_2
-    elif operator == '*':
-        correct_answer = num_1 * num_2
-    return question, str(correct_answer)
+    correct_answer = str(calculate(num_1, num_2, operator))
+    return question, correct_answer
